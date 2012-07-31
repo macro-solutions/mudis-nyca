7b19fc866df0d46cbd82c71a262caca0bc2544d7
cfernandez <cesar.fernandez@macro-solutions.net>
2012-07-31 00:17:41 -0500

    Carga de proyecto a organizacion

---
+++ controllers/login/LoginController.php
@@ -0,0 +1,6 @@
+<?php
+include '../../models/login/Login.php';
+
+
+print_r($_POST);
+?>
---
+++ models/login/Login.php
@@ -0,0 +1,13 @@
+<?php
+
+class Login {
+  
+  private $usr;
+  private $pwd;
+  private $pais;
+  
+  private function __construct(){
+    print_r($_POST);
+  }
+}
+?>
